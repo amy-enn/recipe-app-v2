f17c1cf3353597fb4d88b9fa27214567ae04cec4
amy-enn <amy_enn@protonmail.com>
2024-09-02 16:21:21 -0300

    ingredients mapping out into modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -8,7 +8,7 @@ interface ModalProps {
 
 const Modal = ({ isOpen, onClose }: ModalProps) => {
   const context = useContext(RecipeContext);
-  
+
   if (!context) {
     throw new Error('RecipeContext must be used within a RecipeProvider');
   }
@@ -21,14 +21,28 @@ const Modal = ({ isOpen, onClose }: ModalProps) => {
 
   return (
     <div className="fixed inset-0 bg-cocoa bg-opacity-75 flex items-center justify-center z-50">
+
       <div className="bg-butter shadow-lg p-8 max-w-sm w-full">
         <h2 className="text-2xl font-bold mb-4">{selectedRecipe.recipe_name}</h2>
+
         <p>{selectedRecipe.description}</p>
-        <p className="text-cocoa mb-6">Cupcake ipsum dolor sit amet halvah lemon drops donut sweet roll. Cheesecake cotton candy muffin cake biscuit jelly-o. Marzipan sugar plum cake apple pie cake muffin croissant donut danish.</p>
+
+        <h4>Ingredients</h4>
+        <ul>
+          {selectedRecipe.ingredients.map((ingredient, index) => (
+            <li key={index}>
+              ✦ {ingredient.quantity}{" "}
+              {ingredient.ingredient_name.toLowerCase()}
+            </li>
+          ))}
+        </ul>
+
         <button className="bg-rose border-4 border-cocoa text-black py-1 px-2 rounded hover:scale-105" onClick={onClose}>
           Close
         </button>
+
       </div>
+
     </div>
   )
 }
